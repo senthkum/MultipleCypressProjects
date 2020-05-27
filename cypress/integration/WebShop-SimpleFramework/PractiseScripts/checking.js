fbca2fcb7505ccc6d151ce5d8604d983ed27e788
Senthil <senthkum1822@gmail.com>
2020-05-27 18:48:59 +1000

    added a new script

---
+++ cypress/integration/WebShop-SimpleFramework/PractiseScripts/checking.js
@@ -0,0 +1,47 @@
+/// <reference types="cypress" />
+
+describe('MochaReporter Suit', function()
+{
+
+    it('Launch App 1', function()
+    {
+        //Launch the url
+        cy.visit('http://demowebshop.tricentis.com/')
+        cy.clearCookies()
+        // Enter the login details
+        cy.get('.ico-login').click()
+
+        cy.get('#Email').type('boy@mail.com')
+        cy.get('#Password').type('Boy123')
+        cy.get('.button-1.login-button').click()
+
+    })
+
+    it('Launch App 2', function()
+    {
+        //Launch the url
+        cy.visit('http://demowebshop.tricentis.com/')
+        cy.clearCookies()
+        // Enter the login details
+        cy.get('.ico-login').click()
+
+        cy.get('#Email').type('boy@mail.com')
+        cy.get('#Password').type('Boy123')
+        cy.get('.button-1.login-button').click()
+
+    })
+
+    it('Launch App 3', function()
+    {
+        //Launch the url
+        cy.visit('http://demowebshop.tricentis.com/')
+        cy.clearCookies()
+        // Enter the login details
+        cy.get('.ico-login').click()
+
+        cy.get('#Email').type('boy@mail.com')
+        cy.get('#Password').type('Boy123')
+        cy.get('.button-1.login-button').click()
+
+    })
+})
